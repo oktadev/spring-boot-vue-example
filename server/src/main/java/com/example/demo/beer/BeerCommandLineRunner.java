9ffcc81d52a136f1172f8f3462968338448d0dfc
Matt Raible <matt.raible@okta.com>
2018-11-29 10:17:05 -0700

    Add Spring Boot + Vue example

---
+++ server/src/main/java/com/example/demo/beer/BeerCommandLineRunner.java
@@ -0,0 +1,27 @@
+package com.example.demo.beer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
+
+@Component
+class BeerCommandLineRunner implements CommandLineRunner {
+
+    private final BeerRepository repository;
+
+    public BeerCommandLineRunner(BeerRepository repository) {
+        this.repository = repository;
+        // Top beers from https://www.beeradvocate.com/lists/us, November 2018
+        Stream.of("Kentucky Brunch Brand Stout", "Marshmallow Handjee", "Barrel-Aged Abraxas",
+                "Hunahpu's Imperial Stout", "King Julius", "Heady Topper",
+                "Budweiser", "Coors Light", "PBR").forEach(name ->
+                repository.save(new Beer(name))
+        );
+    }
+
+    @Override
+    public void run(String... strings) {
+        repository.findAll().forEach(System.out::println);
+    }
+}
---
+++ server/src/main/java/com/example/demo/beer/BeerController.java
@@ -0,0 +1,31 @@
+package com.example.demo.beer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+class BeerController {
+
+    private BeerRepository repository;
+
+    public BeerController(BeerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/good-beers")
+    public Collection<Beer> goodBeers() {
+
+        return repository.findAll().stream()
+                .filter(this::isGreat)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Beer beer) {
+        return !beer.getName().equals("Budweiser") &&
+                !beer.getName().equals("Coors Light") &&
+                !beer.getName().equals("PBR");
+    }
+}

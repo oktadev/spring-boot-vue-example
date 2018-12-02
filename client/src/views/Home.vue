7fab3bdc6498781997d408cef601d9cf67858d49
Matt Raible <matt.raible@okta.com>
2018-12-01 21:27:03 -0700

    Fix beer

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <BeerList/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import BeerList from '@/components/BeerList.vue';
 
-@Component
+@Component({
+  components: {BeerList}
+})
 export default class Home extends Vue {}
 </script>
